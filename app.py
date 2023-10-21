d92ce605618b65533f9f4aaf4eb4f73d12b04801
Oduo24 <geraldoduo@gmail.com>
2023-10-21 14:38:30 +0300

    set up the views for routing events and users urls

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask import render_template
 
+
+#Importing the blueprints defined in views
+from views.events.routes import event_views
+from views.users.routes import user_views
+
 app = Flask(__name__)
+
+
+#Registering the blueprints
+app.register_blueprint(event_views)
+app.register_blueprint(user_views)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ views/events/routes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask import render_template, Blueprint
+
+# define the order_views template
+event_views = Blueprint('event_views', __name__,
+                            template_folder='templates',
+                            static_folder='static',
+                            url_prefix='/event')
+
+@event_views.route('/all_events', strict_slashes=False)
+def get_all_events():
+    return "Here is a list of all events"
+
