495947915449b68ec6a8f9591dbf023d89033f98
John Philip <developerphilo@gmail.com>
2023-10-21 17:04:06 +0300

    fix: uuid string generation

--- views/users/model.py
+++ views/users/model.py
@@ -3,7 +3,7 @@ from utils import gen_uuid
 from datetime import datetime
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, default=gen_uuid)
+    id = db.Column(db.String(80), primary_key=True, default=str(gen_uuid))
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     photo = db.Column(db.string(120), nullable=False)
